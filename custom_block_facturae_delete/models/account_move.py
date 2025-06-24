38508b3919d200cabab0ba2b94fd501412f1255a
fondaargentina25 <esteban@eviniegra.software>
2025-06-24 19:30:36 +0200

    update

--- custom_block_facturae_delete/models/account_move.py
+++ custom_block_facturae_delete/models/account_move.py
@@ -4,5 +4,17 @@ from odoo.exceptions import UserError
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    def write(self, vals):
+        for move in self:
+            # Si intenta regresar a borrador
+            if vals.get('state') == 'draft':
+                if move.move_type in ['out_invoice', 'out_refund'] and move.facturae and move.facturae_file_reference:
+                    raise UserError(_("No se puede regresar a borrador una factura que ha sido firmada con Facturae."))
+        return super().write(vals)
+
     def unlink(self):
-        raise UserError(_("No está permitido eliminar facturas en ningún caso."))
+        for move in self:
+            if move.move_type in ['out_invoice', 'out_refund'] and move.facturae and move.facturae_file_reference:
+                raise UserError(_("No se puede eliminar una factura que ha sido firmada con Facturae."))
+        return super().unlink()
+
