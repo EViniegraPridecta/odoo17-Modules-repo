f0ee18aea68ac425b9b53e9992d8a3306da3bca6
Esteban Viniegra <esteban@eviniegra.software>
2025-07-31 16:08:57 +0200

    Update __manifest__.py

--- custom_block_facturae_delete/__manifest__.py
+++ custom_block_facturae_delete/__manifest__.py
@@ -1,11 +1,13 @@
 {
-    'name': 'Bloqueo total de eliminación de facturas',
+    'name': 'Custom Invoice Tools',
     'version': '17.0.1.0.0',
-    'summary': 'Impide eliminar cualquier factura',
+    'summary': 'Patch required for electronic invoice functionality in Spain',
     'author': 'Pridecta',
+    'website': 'https://www.pridecta.es',
     'depends': ['account'],
     'installable': True,
     'application': False,
     'license': 'LGPL-3',
     'icon': 'static/description/icon.png',
 }
+
