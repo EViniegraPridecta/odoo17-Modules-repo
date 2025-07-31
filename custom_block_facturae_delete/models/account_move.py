3fe2741a36c89bfde0b951712ef09b5deeb57a73
Esteban Viniegra <esteban@eviniegra.software>
2025-07-31 16:20:44 +0200

    Update account_move.py

--- custom_block_facturae_delete/models/account_move.py
+++ custom_block_facturae_delete/models/account_move.py
@@ -5,4 +5,8 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def unlink(self):
+        # Permitir solo durante instalación o actualización de módulos
+        if self.env.context.get('install_mode') or self.env.context.get('upgrade_mode'):
+            return super().unlink()
+
         raise UserError(_("No está permitido eliminar facturas. Si necesita anular una factura, utilice la opción de anulación."))
